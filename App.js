8d16bd8e3983cc21f8871c01e1f9310bdba88f38
salaama2021 <134949692+salaama2021@users.noreply.github.com>
2023-06-02 05:23:09 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <head></head>
+      <body>
+        <div className="flexbox-container">
+          <div className="flexbox-item flexbox-item-1"></div>
+          <div className="flexbox-item flexbox-item-2"></div>
+          <div className="flexbox-item flexbox-item-3"></div>
+          </div>
+         
+         <div className="flexbox-square">
+            <div className="flexbox-box flexbox-box-1"></div>
+            <div className="flexbox-box flexbox-box-2"></div>
+            <div className="flexbox-box flexbox-box-3"></div>
+            </div>
+
+            <div className="flexbox-line">
+              <div className="flexbox-big flexbox-big-1"></div>
+            </div>
+            
+            <div className="flexbox-one">
+              <div className="flexbox-row flexbox-row-1"></div>
+            </div>
+        </body>
+        </div>
+  );
+}
+
+export default App;
